ae5c2ced772dd70383e40deeb9237395f660ff1d
neliagonzalez95 <93960399+neliagonzalez95@users.noreply.github.com>
2021-11-08 21:10:42 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDeyIDSI4CfR8C99QEy1EXDKZIc1-mzDNI",
+  authDomain: "itic1001.firebaseapp.com",
+  projectId: "itic1001",
+  storageBucket: "itic1001.appspot.com",
+  messagingSenderId: "1038665196071",
+  appId: "1:1038665196071:web:ad1274253d799351396402",
+  measurementId: "G-B23F568BRV",
+});
